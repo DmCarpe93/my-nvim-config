367756030390facd4c2703a560643bb2b5b298ee
DmCarpe93 <ladongmin0619@gmail.com>
2026-07-01 13:36:10 +0900

    fixed errors

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -15,6 +15,10 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+-- copy and paste
+vim.opt.mouse = "nvi"
+vim.opt.clipboard = "unnamedplus"
+
 -- Make sure to setup `mapleader` and `maplocalleader` before
 -- loading lazy.nvim so that mappings are correct.
 -- This is also a good place to setup other settings (vim.opt)
